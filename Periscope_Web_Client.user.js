dde1d70e70a5651628b4d27fe318e731fc12937c
max104t <max104t_2000@hotmail.com>
2019-08-16 14:00:10 -0700

    use gitnew2018 repository

--- Periscope_Web_Client.user.js
+++ Periscope_Web_Client.user.js
@@ -19,8 +19,8 @@
 // @require     https://github.com/iamcal/js-emoji/raw/master/lib/emoji.js
 // @require     https://github.com/zenorocha/clipboard.js/raw/v2.0.0/dist/clipboard.min.js
 // @require     https://github.com/le717/jquery-spoiler/raw/master/jquery.spoiler.min.js
-// @require     https://raw.githubusercontent.com/Max104t/My-OpenPeriscope/split-refactor-api/ApiTest.js
-// @require     https://raw.githubusercontent.com/Max104t/My-OpenPeriscope/split-refactor-api/PeriscopeApiWrapper.js
+// @require     https://github.com/gitnew2018/My-OpenPeriscope/raw/master/ApiTest.js
+// @require     https://github.com/gitnew2018/My-OpenPeriscope/raw/master/PeriscopeApiWrapper.js
 // @require     https://unpkg.com/split.js/dist/split.min.js
 // @downloadURL https://github.com/gitnew2018/My-OpenPeriscope/raw/master/Periscope_Web_Client.user.js
 // @updateURL   https://github.com/gitnew2018/My-OpenPeriscope/raw/master/Periscope_Web_Client.meta.js
@@ -28,14 +28,14 @@
 // @noframes
 // @grant       GM_addStyle
 // @grant       GM_getResourceText
-// @resource    CSS https://raw.githubusercontent.com/Max104t/My-OpenPeriscope/split-refactor-api/style.css
+// @resource    CSS https://github.com/gitnew2018/My-OpenPeriscope/raw/master/style.css
 
 // ==/UserScript==
 
 var emoji = new EmojiConvertor();
 /* drkchange06 */var childProcesses=[]; //list of video downloading processes
 /* drkchange03 */var selectedDownloadList = localStorage.getItem('selectedUsersDownloadList') || "";
-var IMG_PATH = 'https://raw.githubusercontent.com/Max104t/My-OpenPeriscope/split-refactor-api';
+var IMG_PATH = 'https://github.com/gitnew2018/My-OpenPeriscope/raw/master';
 var settings = JSON.parse(localStorage.getItem('settings')) || {};
 const NODEJS = typeof require === 'function';
 if (NODEJS) {  // for NW.js
